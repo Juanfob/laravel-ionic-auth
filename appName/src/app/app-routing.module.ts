cf80306f81dabca6c534fcecec1aad313d31c412
Juan Fermin <juanfob.dev@gmail.com>
2020-07-31 21:07:16 +0200

    subida total del ejemplo de Autenticacion desde Ionic a bakend con Laravel

---
+++ appName/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+
+
+
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' },  
+  { path: 'login', loadChildren: './pages/auth/login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './pages/auth/register/register.module#RegisterPageModule' },
+  
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuard] },
+  //{ path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard] },
+  //{ path: 'list', loadChildren: './list/list.module#ListPageModule', canActivate: [AuthGuard] },
+
+
+
+
+  //{
+  //  path: 'folder/:id',
+  //  loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  //},
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
